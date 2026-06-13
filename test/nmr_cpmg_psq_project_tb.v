eb800dea6c68269cfecf202899cf3e84b133ff3f
thanusit <72438474+thanusit@users.noreply.github.com>
2026-06-13 16:39:26 +0700

    Update nmr_cpmg_psq_project_tb.v

--- test/nmr_cpmg_psq_project_tb.v
+++ test/nmr_cpmg_psq_project_tb.v
@@ -1,6 +1,6 @@
 `timescale 1ns / 1ps
 
-module tb;
+module tt_um_thanusit_cpmg_pulse_sequencer_tb;
 
     // Testbench signals
     reg [7:0] ui_in;
@@ -60,8 +60,8 @@ module tb;
         ui_in  = 8'h08; // SS_N initialized high, all others low
         uio_in = 8'h00;
 
-        $dumpfile("tb.vcd");
-        $dumpvars(0, tb);
+        $dumpfile("tt_um_thanusit_cpmg_pulse_sequencer_tb.vcd");
+        $dumpvars(0, tt_um_thanusit_cpmg_pulse_sequencer_tb);
 
         // Reset Sequence
         #100;
