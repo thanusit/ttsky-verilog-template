7853b270996d25d0240dd2561b300ce65a89c950
thanusit <72438474+thanusit@users.noreply.github.com>
2026-06-13 21:27:49 +0700

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,31 +1,43 @@
 # SPDX-FileCopyrightText: © 2024 Tiny Tapeout
 # SPDX-License-Identifier: Apache-2.0
 
-import cocotb
-from cocotb.clock import Clock
-from cocotb.triggers import ClockCycles
+# import cocotb
+# from cocotb.clock import Clock
+# from cocotb.triggers import ClockCycles
 
 
-@cocotb.test()
-async def test_project(dut):
-    dut._log.info("Start")
+# @cocotb.test()
+# async def test_project(dut):
+#    dut._log.info("Start")
 
     # Set the clock period to 10 us (100 KHz)
-    clock = Clock(dut.clk, 10, unit="us")
-    cocotb.start_soon(clock.start())
+#    clock = Clock(dut.clk, 10, unit="us")
+#    cocotb.start_soon(clock.start())
 
     # Reset
-    dut._log.info("Reset")
-    dut.ena.value = 1
-    dut.ui_in.value = 0
-    dut.uio_in.value = 0
-    dut.rst_n.value = 0
-    await ClockCycles(dut.clk, 10)
-    dut.rst_n.value = 1
+#    dut._log.info("Reset")
+#    dut.ena.value = 1
+#    dut.ui_in.value = 0
+#    dut.uio_in.value = 0
+#    dut.rst_n.value = 0
+#    await ClockCycles(dut.clk, 10)
+#   dut.rst_n.value = 1
 
-    dut._log.info("Test project behavior")
+#    dut._log.info("Test project behavior")
 
-    # Set the input values you want to test
+import cocotb
+from cocotb.triggers import Timer
+
+@cocotb.test()
+async def run_verilog_for_fixed_duration(dut):
+    """
+    Waits for a predefined simulation timeframe to give 
+    tb.v enough space to complete all its internal operations.
+    """
+    # Adjust 10000 to match your testbench duration requirements
+    await Timer(100000, units="ns") 
+
+# Set the input values you want to test
     #dut.ui_in.value = 20
     #dut.uio_in.value = 30
 
